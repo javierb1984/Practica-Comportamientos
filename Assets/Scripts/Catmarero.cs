9ba9e768431cacf02a23cdd47cd5391d41e20b4a
javihollow <javierbravo1998@outlook.com>
2019-11-11 16:21:10 +0100

    initial commit

---
+++ Assets/Scripts/Catmarero.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catmarero : Gato {
+
+	void Start () {
+
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Gato.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gato : MonoBehaviour {
+
+	private void walkTo(Vector3 destination){}
+
+	private void runTo(Vector3 destination){}
+
+	private void pick(/*Item item*/){}
+
+	private void idle(){}
+
+	private void sitDown(Vector3 lookAt){}
+
+	private void getUp(){}
+
+	private void eat(/*Item food*/){}
+
+	private void cook(/*Item food*/){}
+
+	private void play(/*Item item*/){}
+
+	private void angry(Vector3 lookAt){}
+
+	private void shamed(Vector3 lookAt){}
+
+}
