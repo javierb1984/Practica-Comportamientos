2b19965dbda29cedf1354cd875fd1ac83d54b57a
Laura Suonperä Lozano <43136220+LauraLuna96@users.noreply.github.com>
2019-11-24 15:24:05 +0100

    Texturas aleatorias en los clientes (solo script)
    hay que asociarlo

---
+++ Assets/Scripts/RandomSkin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSkin : MonoBehaviour
+{
+    public List<Texture2D> PossibleTextures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = Random.Range(0, PossibleTextures.Count);
+        Texture2D skin = PossibleTextures[i];
+
+        GetComponent<SkinnedMeshRenderer>().material.mainTexture = skin;
+    }
+}
